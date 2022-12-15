88bc1030cd3c12f250cf7d9d61f6a6f78522ffb8
LaraeLua <111380182+LaraeLua@users.noreply.github.com>
2022-12-15 14:48:24 -0300

    Update form_editar.php

--- form_editar.php
+++ form_editar.php
@@ -7,7 +7,8 @@
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 </head>
-<body>
+<body style="text-align: center; background: rgb(254,255,13);
+background: linear-gradient(90deg, rgba(254,255,13,1) 0%, rgba(230,208,0,1) 35%, rgba(255,132,0,1) 100%);">
 	<?php 
 
 		include_once 'conexao.php';
@@ -27,23 +28,50 @@
 		<div class="container col-sm-6">
 			<div class="panel panel-default">
 				<div class="panel-body">
-					<div class="panel-heading text-center display-4">ALTERAR DADOS</div>
+					<div style="color: #bb1111; font-family: Arial;" class="panel-heading text-center display-4"><h2>ALTERAR DADOS</h2></div>
 
     				<form action="alterar.php?id=<?php echo $id;?>"
     						 method="post" accept-charset="utf-8" class="form-group">
 
-    					<label for="nome">Nome:</label>
+    					<label style="color: #bb1111; font-family: Arial;"" for="nome">Nome:</label>
     					<input type="text" name="nome" value="<?php echo $nome;?>" class="form-control">
-    						
-    					<label for="endereco">Endereço:</label>
-    					<input type="text" name="endereco" value="<?php echo $endereco;?>" class="form-control">
-
-    					<label for="pedido">Pedidos:</label>
-    					<input type="text" name="pedido" value="<?php echo $pedido;?>" class="form-control">
-
     					<br>
-    					<input type="submit" name="btn" value="ALTERAR" class="btn btn-success">
-    					<a href="mostrar.php" class="btn red">Cancelar</a>
+                        <br>
+    					<label style="color: #bb1111; font-family: Arial;" for="endereco">Endereço:</label>
+    					<input type="text" name="endereco" value="<?php echo $endereco;?>" class="form-control">
+                        <br>
+                        <br>
+    					<label style="color: #bb1111; font-family: Arial;" for="pedido">Pedidos:</label>
+    					<input type="text" name="pedido" value="<?php echo $pedido;?>" class="form-control">
+                        <br>
+    					<br>
+                        <br>
+    					<input style="box-shadow:inset 0px 1px 0px 0px #cf866c;
+                                background:linear-gradient(to bottom, #bb1111 5%, #bc3315 100%);
+                                background-color:#bb1111;
+                                border-radius:9px;
+                                border:1px solid #942911;
+                                display:inline-block;
+                                cursor:pointer;
+                                color:#ffffff;
+                                font-family:Arial;
+                                font-size:13px;
+                                padding:9px 24px;
+                                text-decoration:none;
+                                text-shadow:0px 1px 0px #854629;" type="submit" name="btn" value="ALTERAR" class="btn btn-success">
+    					<a style="box-shadow:inset 0px 1px 0px 0px #cf866c;
+                                background:linear-gradient(to bottom, #bb1111 5%, #bc3315 100%);
+                                background-color:#bb1111;
+                                border-radius:9px;
+                                border:1px solid #942911;
+                                display:inline-block;
+                                cursor:pointer;
+                                color:#ffffff;
+                                font-family:Arial;
+                                font-size:13px;
+                                padding:9px 24px;
+                                text-decoration:none;
+                                text-shadow:0px 1px 0px #854629;" href="mostrar.php" class="btn red">Cancelar</a>
 
     				</form>
 				</div>
